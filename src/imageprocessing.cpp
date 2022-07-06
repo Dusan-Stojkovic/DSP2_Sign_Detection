fdca1efad07cb087e05b89795335f553fd5a31f4
pavko00 <pavkovicigor7@gmail.com>
2022-07-06 19:04:20 +0200

    Detecting corners of contours and performing perspective transformation

--- src/imageprocessing.cpp
+++ src/imageprocessing.cpp
@@ -56,6 +56,11 @@ void SignDetection::parse(cv::Mat& im)
 	cv::imshow("contours", contourImage);
 
 	//approximate shape of contour
+	std::vector<cv::Point> exLeft;
+	std::vector<cv::Point> exRight;
+	std::vector<cv::Point> exTop;
+	std::vector<cv::Point> exBott;
+
 	int order = 0;
 	double arclen = 0;
 	double epsilon = 0;
@@ -67,12 +72,96 @@ void SignDetection::parse(cv::Mat& im)
 		epsilon = 0.01*arclen;
 		cv::approxPolyDP(*contour, approx_curve, epsilon, true);
 		order = approx_curve.size();
-		if(order > 15)
+		if(order > 12 && order < 17)
 		{
 			std::cout << "Red circle!\n";
-		}
-	}
+			
+			//get corners of contours and perform perspective transformation
+			
+			cv::Point tmpLeft;
+			cv::Point tmpRight;
+			cv::Point tmpTop;
+			cv::Point tmpBott;
+			tmpLeft.x = 5000;
+			tmpRight.x = -5000;
+			tmpBott.y = -5000;
+			tmpTop.y = 5000;
 
+			std::vector<cv::Point> cnt;
+			cnt = *contour;
+			for (size_t j=0; j < cnt.size();j++){
+				cv::Point current = cnt[j];
+				if (current.x < tmpLeft.x){
+					tmpLeft = current;
+				}
+				if (current.x > tmpRight.x){
+					tmpRight = current;
+				}
+				if (current.y > tmpBott.y){
+					tmpBott = current;
+				}
+				if (current.y < tmpTop.y){
+					tmpTop = current;
+				}
+			}
+			exLeft.push_back(tmpLeft);
+			exRight.push_back(tmpRight);
+			exTop.push_back(tmpTop);
+			exBott.push_back(tmpBott);
+
+		
+			int farL = exLeft[0].x;
+			int farR = exRight[0].x;
+			int farT = exTop[0].y;
+			int farB = exBott[0].y;
+			
+			for(int i = 0; i < exLeft.size();i++){
+				if(farL > exLeft[i].x){
+					farL = exLeft[i].x;
+				}
+			}
+			for(int i = 0; i < exRight.size();i++){
+				if(farR < exRight[i].x){
+					farR = exRight[i].x;
+				}
+			}
+			for(int i = 0; i < exTop.size();i++){
+				if(farL > exTop[i].y){
+					farL = exTop[i].y;
+				}
+			}
+			for(int i = 0; i < exBott.size();i++){
+				if(farB < exBott[i].y){
+					farB = exBott[i].y;
+				}
+			}
+			
+			std::vector<cv::Point2f> src;
+			src.push_back(cv::Point2f( (float)(farL),(float)(farT)));
+			src.push_back(cv::Point2f( (float)(farR),(float)(farT)));
+			src.push_back(cv::Point2f( (float)(farL),(float)(farB)));
+			src.push_back(cv::Point2f( (float)(farR),(float)(farB)));
+			
+			
+			std::vector<cv::Point2f> dst;
+			dst.push_back(cv::Point2f( (float)(0),(float)(0)));
+			dst.push_back(cv::Point2f( (float)(farR),(float)(0)));
+			dst.push_back(cv::Point2f( (float)(0),(float)(farB)));
+			dst.push_back(cv::Point2f( (float)(farR),(float)(farB)));
+			
+
+			cv::Mat M = cv::getPerspectiveTransform(src,dst);
+
+			cv::Size warped_image_size = cv::Size(farR+15,farB+15); // +15 is for padding of the sign
+
+			cv::Mat warpedImg;
+			cv::warpPerspective(im,warpedImg,M,warped_image_size);
+
+			cv::imshow("Warped",warpedImg);
+		}
+
+	}
+	
 	cv::waitKey();
 }
 
